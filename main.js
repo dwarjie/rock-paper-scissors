a42a2c293ecbc69c0e50d425d215b3739423dcfa
dwarjie <mozencode@gmail.com>
2020-08-12 12:54:57 +0800

    Picking cards done

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-// 1 - Rock
-// 2 - Paper
-// 3 - Scissors
+// 1 - 3
+// 2 - 1
+// 3 - 2
+const cards = document.querySelector('.left-choices');
+
+cards.addEventListener('click', checkPick);
 
 // main game function 
 function game() {
@@ -12,6 +15,21 @@ function game() {
 	}
 }
 
+// click events for the cards
+function checkPick(e) {
+	const item = e.target;
+	let playerChoice;
+	if (item.classList[2] == 'card-rock') {
+		playerChoice = 3;
+	} else if (item.classList[2] == 'card-paper') {
+		playerChoice = 1;
+	} else { 
+		// scissors
+		playerChoice = 2;
+	}
+	console.log(start(playerChoice, computerPlay()));
+}
+
 // randomly pick from the game choices for computer AI
 function computerPlay() {
 	const GAME_CHOICE = [1, 2, 3];
@@ -20,25 +38,25 @@ function computerPlay() {
 
 function start(playerSelection, computerSelection) {
 	let player;
-	switch (playerSelection.toLowerCase()) {
-		case 'rock':
-			player = 1;
-			break;
-		case 'paper':
-			player = 2;
-			break;
-		case 'scissor':
-			player = 3;
-			break;
-	}
-
-	if (player == computerSelection) {
+	// switch (playerSelection.toLowerCase()) {
+	// 	case 'rock':
+	// 		player = 1;
+	// 		break;
+	// 	case 'paper':
+	// 		player = 2;
+	// 		break;
+	// 	case 'scissor':
+	// 		player = 3;
+	// 		break;
+	// }
+	console.log(computerSelection);
+	if (playerSelection == computerSelection) {
 		return 'DRAW';
-	} else if (player == 1 && computerSelection == 2) {
+	} else if (playerSelection == 1 && computerSelection == 2) {
 		return 'You LOSE!';
-	} else if (player == 2 && computerSelection == 3) {
+	} else if (playerSelection == 2 && computerSelection == 3) {
 		return 'You SUCK!';
-	} else if (player == 3 && computerSelection == 1) {
+	} else if (playerSelection == 3 && computerSelection == 1) {
 		return 'You FUCK!';
 	} else {
 		return 'YOU ROCK!'; // you win
